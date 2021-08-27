2702a42461f906a26faa956c844af59aff5f8613
Robert <robert.thornton92@gmail.com>
2021-08-27 15:28:44 +0200

    Added more keyboard support

--- buttonActions.js
+++ buttonActions.js
@@ -1,27 +1,36 @@
 import { operate } from './operations.js';
 
-const input = document.querySelector("div.input-container input[type='text']");
+const input = document.querySelector("#mainInput");
+const secondaryInput = document.querySelector('#secondaryInput');
 const equals = document.querySelector('.equal');
 const clearInput = document.querySelector('.clearInput');
 const remove = document.querySelector('.remove');
 const numbersBox = document.querySelector('.numbers').querySelectorAll('button');
 const operandsBox = document.querySelector('.operands').querySelectorAll('button');
 const decimal = document.querySelector('div.numbers button.decimal');
+let equated = false;
 
 let addNumber = function getContent(event) {
-    input.value += event.currentTarget.textContent;
+    if (equated) { 
+        clearInput.click();
+        equated = false; 
+    }
+    input.textContent += event.currentTarget.textContent;
 };
 
 let addOperand = function getOperand(event) {
-    input.value += event.currentTarget.textContent;
-    if(input.value.includes('.')) {
-        decimal.addEventListener('click', addDecimal, true);
+    let string = input.textContent;
+    secondaryInput.textContent += `${string}${event.currentTarget.textContent}`;
+    input.textContent = ''
+    if(!input.textContent.includes('.')) {
+         decimal.addEventListener('click', addDecimal, true);
     }
+
 }
 
 let addDecimal = function getValue(event) {
-    input.value += event.currentTarget.textContent;
-    if (input.value.includes('.')){
+    input.textContent += event.currentTarget.textContent;
+    if (input.textContent.includes('.')){
         decimal.removeEventListener('click', addDecimal, true);
     }
 }
@@ -40,40 +49,57 @@ decimal.addEventListener('click', addDecimal, true);
 let timer;
 remove.addEventListener('mousedown', () => {
     timer = setInterval(() => {
-        let string = input.value.slice(0,-1);
-        input.value = string
+        let string = input.textContent.slice(0,-1);
+        input.textContent = string
     }, 100);
 });
 remove.addEventListener('mouseup', () => {
     if (timer) clearInterval(timer);
 });
 remove.addEventListener('click', () => {
-    let string = input.value.slice(0,-1);
-    input.value = string
+    let string = input.textContent.slice(0,-1);
+    input.textContent = string
 });
 
 clearInput.addEventListener('click', () => {
-    input.value = '';
+    input.textContent = '';
+    secondaryInput.textContent = '';
     decimal.removeEventListener('click', addDecimal, true);
     decimal.addEventListener('click', addDecimal, true);
 });
 
 equals.addEventListener('click', () => {
-    let string = input.value;
+    let string = secondaryInput.textContent + input.textContent;
+    secondaryInput.textContent = string;
     let returnedValue = operate(string);
-    input.value = returnedValue;
+    input.textContent = returnedValue;
     decimal.addEventListener('click', addDecimal, true);
-    
+    equated = true;
 });
 
+//////////// Keyboard support ////////////
 document.addEventListener('keydown', (e) => {
-    //console.log(e.key);
     let back = /(backspace)/i;
-    let regex = /([(\d)(+)(\-)(*)(/)])/;
-    if (regex.test(e.key)) { input.value += e.key}
-    else if (back.test(e.key)) {
-        console.log(e.key);
-        let string = input.value.slice(0,-1);
-        input.value = string;
+    let operator = /([(+)(\-)(*)(/)])/;
+    let number = /([(\d)(.)])/;
+    let enter = /(enter)/i;
+    if (number.test(e.key)) { 
+        if (equated) { clearInput.click(); equated = false; input.textContent += e.key; }
+        else if (e.key === '.') { decimal.click(); }
+        else { input.textContent += e.key; }
     }
+    else if (operator.test(e.key)) {
+        if (equated) { clearInput.click(); equated = false; input.textContent = e.key;}
+            let string = input.textContent;
+            secondaryInput.textContent += `${string}${e.key}`;
+            input.textContent = ''
+            if(!input.textContent.includes('.')) {
+                decimal.addEventListener('click', addDecimal, true);
+            }
+        }
+    else if (back.test(e.key)) {
+        let string = input.textContent.slice(0,-1);
+        input.textContent = string;
+    } 
+    else if (enter.test(e.key)) equals.click();
 });
