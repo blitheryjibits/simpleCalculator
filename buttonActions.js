d573fde084ce27e08820a62720ecf05f809d3189
Robert <robert.thornton92@gmail.com>
2021-08-27 11:05:24 +0200

    Changed eventListeners to remove decimal eventListener after being pressed and operators or equals to set eventListener on decimal. This limits each number to having only 1 decimal at a time.

--- buttonActions.js
+++ buttonActions.js
@@ -3,36 +3,66 @@ import { operate } from './operations.js';
 const input = document.querySelector("div.input-container input[type='text']");
 const equals = document.querySelector('.equal');
 const clearInput = document.querySelector('.clearInput');
+const remove = document.querySelector('.remove');
 const numbersBox = document.querySelector('.numbers').querySelectorAll('button');
 const operandsBox = document.querySelector('.operands').querySelectorAll('button');
+const decimal = document.querySelector('div.numbers button.decimal');
 
-// adds button values to the input field
- numbersBox.forEach(box => {
-    box.addEventListener('click', (e) => {
-        input.value += e.currentTarget.textContent; 
-    });
+let addNumber = function getContent(event) {
+    input.value += event.currentTarget.textContent;
+};
+
+let addOperand = function getOperand(event) {
+    input.value += event.currentTarget.textContent;
+    if(input.value.includes('.')) {
+        decimal.addEventListener('click', addDecimal, true);
+    }
+}
+
+let addDecimal = function getValue(event) {
+    input.value += event.currentTarget.textContent;
+    if (input.value.includes('.')){
+        decimal.removeEventListener('click', addDecimal, true);
+    }
+}
+
+numbersBox.forEach(box => {
+    box.addEventListener('click', addNumber, true)
 });
 
 operandsBox.forEach(box => {
-    box.addEventListener('click', (e) => {
-        input.value += e.currentTarget.textContent; 
-    });
+    box.addEventListener('click', addOperand, true)
 });
 
+decimal.removeEventListener('click', addNumber, true);
+decimal.addEventListener('click', addDecimal, true);
+
 let timer;
-clearInput.addEventListener('mousedown', () => {
+remove.addEventListener('mousedown', () => {
     timer = setInterval(() => {
         let string = input.value.slice(0,-1);
         input.value = string
     }, 100);
 });
-clearInput.addEventListener('mouseup', () => {
+remove.addEventListener('mouseup', () => {
     if (timer) clearInterval(timer);
+});
+remove.addEventListener('click', () => {
+    let string = input.value.slice(0,-1);
+    input.value = string
+});
+
+clearInput.addEventListener('click', () => {
+    input.value = '';
+    decimal.removeEventListener('click', addDecimal, true);
+    decimal.addEventListener('click', addDecimal, true);
 });
 
 equals.addEventListener('click', () => {
     let string = input.value;
     let returnedValue = operate(string);
     input.value = returnedValue;
+    decimal.addEventListener('click', addDecimal, true);
     
 });
+
